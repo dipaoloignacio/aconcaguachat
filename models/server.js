7baf2d4a4eed4d3078cd9af2f3e0a13f4d935a4c
Ignacio Di Paolo <nachobeatbox@gmail.com>
2022-10-26 19:43:10 -0300

    first commit

--- models/server.js
+++ models/server.js
@@ -11,7 +11,7 @@ class Server {
 
     constructor() {
         this.app = express();
-        this.port = process.env.PORT || 5000;
+        this.port = process.env.PORT;
 
         //conectar a DB
         dbConection();
